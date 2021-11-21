1016072ffb7b337768514b2dbf9fd048ed715b4e
Alessandra Lamas Luisi <lecaluisi@hotmail.com>
2021-11-21 13:48:50 -0300

    Tratando a tag input

---
+++ input/index.ts
@@ -0,0 +1,6 @@
+const input = document.getElementById("input") as HTMLInputElement;
+
+input.addEventListener("input", (event) => {
+    const i = event.currentTarget as HTMLInputElement;
+    console.log(i.value);
+});
