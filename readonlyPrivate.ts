4acb91e0055a681652dae913e39ee680ffdd060c
Alessandra Lamas Luisi <lecaluisi@hotmail.com>
2021-11-21 14:41:35 -0300

    Criando variáveis com prop readonly e private

---
+++ readonlyPrivate.ts
@@ -0,0 +1,22 @@
+interface Cachorro {
+    nome: string;
+    idade: number;
+    parqueFavorito?: string;
+}
+
+type CachorroSomenteLeitura = {
+    +readonly [K in keyof Cachorro]-?: Cachorro[K];
+}
+
+class MeuCachorro implements CachorroSomenteLeitura {
+    idade;
+    nome;
+    parqueFavorito;
+
+    constructor(nome, idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+}
+
+const cao = new MeuCachorro("Greg", 1);
